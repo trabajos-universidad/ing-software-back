dd529a7cecf1e7ad8982aa425083222a5bdb9e52
Chino <victort8012@gmail.com>
2022-07-13 18:19:41 -0400

    agregando proteccion de rutas

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,21 @@
 const { Router } = require('express');
 const { login, register } = require('../controller/auth');
-
+const validarCampos = require('../middleware/validar-campos');
+const { check } = require('express-validator');
 
 const router = Router();
 
-router.post ('/register', register);
+router.post ('/register', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validarCampos
+], register);
 
-router.post('/', login);
+router.post('/', [
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    validarCampos
+],login);
 
 module.exports = router;
--- routes/productos.js
+++ routes/productos.js
@@ -1,11 +1,21 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getProductos, crearProducto, deleteProducto } = require('../controller/productos');
+const validarCampos = require('../middleware/validar-campos');
 const router = Router();
 
 router.get('/', getProductos)
 
-router.post('/crear-productos', crearProducto)
+router.post('/crear-productos',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('foto', 'La foto es obligatoria').not().isEmpty(),
+    check('desc', 'La Descricion es obligatoria').not().isEmpty(),
+    validarCampos
+] ,crearProducto)
 
-router.delete('/:id', deleteProducto);
+router.delete('/:id', [
+    check('id', 'El id es obligatorio').not().isEmpty(),
+    validarCampos
+] ,deleteProducto);
 
 module.exports = router;
